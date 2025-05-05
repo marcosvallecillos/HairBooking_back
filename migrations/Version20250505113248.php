a32d9cbe7499e3adc7791246c3804f4b2da21f99
Marcos Vallecillos <mavaus@floridauniversitaria.es>
2025-05-05 15:30:37 +0200

    arreglando funcion para crear valoraciones

--- migrations/Version20250505113248.php
+++ migrations/Version20250505113248.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250505112015 extends AbstractMigration
+final class Version20250505113248 extends AbstractMigration
 {
     public function getDescription(): string
     {
